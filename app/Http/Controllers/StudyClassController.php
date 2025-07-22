04b2acef1e3f88bab2356c48c70cb8a67dcf8475
adam mac <yaufaniadam@gmail.com>
2025-07-22 17:30:29 +0700

    kelas

--- app/Http/Controllers/StudyClassController.php
+++ app/Http/Controllers/StudyClassController.php
@@ -23,7 +23,7 @@ class StudyClassController extends Controller
     public function show(StudyClass $studyClass)
     {
         // Eager load relasi yang dibutuhkan untuk efisiensi
-        $studyClass->load('students');
+        $studyClass->load('students', 'subject.course');
 
         // 1. Ambil ID program dari kelas yang sedang dilihat
         $classCourseId = $studyClass->subject->course_id;
